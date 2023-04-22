7985cb9f9c50180ddfe28109e6081afb0b436342
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-04-21 21:25:08 -0600

    opcion 3

--- FuncionesLabMat.py
+++ FuncionesLabMat.py
@@ -21,7 +21,7 @@ def validacionMonto(monto,edificio,piso,local):
     estado=True
     while estado:
         if re.match('^\d+$',str(monto)):
-            if monto!=edificio[int(piso)][int(local)]:
+            if monto!=edificio[int(piso)-1][int(local)-1]:
                 return True
         else:
             print('El monto ingresado debe ser un número entero positivo')
@@ -36,12 +36,12 @@ def generarMatriz(filas,columnas):
     return matriz
 
 def determinarDisponLocal(piso,local,matriz):
-    if matriz[int(piso)][int(local)]==0:
+    if matriz[int(piso)-1][int(local)-1]==0:
         return True
     return False
 
 def definirRenta(piso,local,alquiler,matriz):
-    matriz[piso][local]=alquiler
+    matriz[int(piso)-1][int(local)-1]=alquiler
     return matriz
 
 def opcion1Aux(edificio):
@@ -62,7 +62,7 @@ def opcion2Aux(edificio):
     estado=True
     while estado:
         piso=input("Ingrese el piso en el que se encuentra el local a modificar: ")
-        local=input("Ingrese el número del local a alquilar: ")
+        local=input("Ingrese el número del local a modificar: ")
         if determinarDisponLocal(piso,local,edificio)==False:
             contesta=contestacion('¿Está seguro que desea alterar la renta actual del local?')
             if not contesta:
@@ -74,12 +74,27 @@ def opcion2Aux(edificio):
                     print('¡El alquiler del local fue modificado satisfactoriamente!')
                     estado=contestacion('¿Desea continuar modificando alquileres?')
         else:
-            print("¡¡¡Algo anda mal!!!\n El local no se encuentra disponible\nPor favor ingrese un numero de local disponible")
+            print("¡¡¡Algo anda mal!!!\n El local no se encuentra alquilado\nPor favor ingrese un numero de local que pueda modificarse")
     return edificio
 
 def opcion3Aux(edificio):
-    return ''
-
+    estado=True
+    while estado:
+        piso=input("Ingrese el piso en el que se encuentra el local a desalojar: ")
+        local=input("Ingrese el número del local a desalojar: ")
+        if determinarDisponLocal(piso,local,edificio)==False:
+            contesta=contestacion('¿Está seguro que desea desalojar este local?')
+            if not contesta:
+                estado=contestacion('¿Desea continuar desalojando locales?')
+            else:
+                monto=0
+                edificio=definirRenta(piso,local,monto,edificio)
+                print("El local se ha desalojado de manera correcta")
+                estado=contestacion('¿Desea desalojar otro local?')
+        else:
+            print("¡¡¡Algo anda mal!!!")
+            print("\nEl local no se encuentra alquilado\nPor favor ingrese un numero de local que si esté alquilado, para modificarlo")
+    return edificio
 
 def EyS():
     print("Bienvenido al Sistema de Administración de Locales".center(90,"="))
@@ -108,7 +123,7 @@ def EyS():
         
 
 
-print(opcion2Aux([[0,1,0],[0,0,0],[0,0,0]]))
+print(opcion3Aux([[0,1,0],[0,0,0],[0,0,0]]))
 
 
 
